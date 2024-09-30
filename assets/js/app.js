324ab81d66b8cccc22ccefe3ded9cd8815ad0415
NUTI\kaos <osakahcmc@gmail.com>
2024-09-30 17:34:14 +0700

    feat(gallery): final

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,11 @@ async function loadData(request, templateId, viewId) {
     view.innerHTML = template(context);
     // console.log(html);
 }
+
+
+function myFunction(e) {
+    if (document.querySelector('.navList a.active') !== null) {
+      document.querySelector('.navList a.active').classList.remove('active');
+    }
+    e.target.className = "active";
+  }
